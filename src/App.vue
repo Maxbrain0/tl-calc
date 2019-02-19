6385c480a877cd4fc7b18a68efaaabf7487115ba
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-02-19 12:58:00 -0800

    Abbreviate title for very small screens

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,32 @@
   <div id="app">
     <v-app>
       <v-toolbar class="toolbar" dark color="primary">
-        <v-toolbar-title>Line Impedance Calculator</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-title v-if="!isMobile">Line Impedance Calculator</v-toolbar-title>
+        <v-toolbar-title v-else>TL Calc</v-toolbar-title>
+        <v-spacer></v-spacer>
         <v-toolbar-items>
-          <info-dialog></info-dialog>
+          <v-btn icon>
+            <info-dialog></info-dialog>
+          </v-btn>
         </v-toolbar-items>
       </v-toolbar>
       <v-container>
         <v-layout justify-center>
           <v-flex xs10 sm8 lg6>
-           <v-select
-            :items="tlItems"
-            item-text="name"
-            item-value="component"
-            v-model="tlType"
-            label="Transmission Line Type"
-            single-line
-            color="secondary"
-          ></v-select>
-        </v-flex>
+            <v-select
+              :items="tlItems"
+              item-text="name"
+              item-value="component"
+              v-model="tlType"
+              label="Transmission Line Type"
+              single-line
+              color="secondary"
+            ></v-select>
+          </v-flex>
         </v-layout>
         <keep-alive>
-          <component :is="tlType"></component> 
+          <component :is="tlType"></component>
         </keep-alive>
       </v-container>
     </v-app>
@@ -30,26 +35,27 @@
 </template>
 
 <script>
-import Coax from './components/Coax.vue';
-import Microstrip from './components/Microstrip.vue';
-import SymmStripline from './components/SymmStripline.vue';
-import CPW from './components/CPW.vue';
-import GCPW from './components/GCPW.vue';
-import InfoDialog from './components/InfoDialog.vue';
+import Coax from "./components/Coax.vue";
+import Microstrip from "./components/Microstrip.vue";
+import SymmStripline from "./components/SymmStripline.vue";
+import CPW from "./components/CPW.vue";
+import GCPW from "./components/GCPW.vue";
+import InfoDialog from "./components/InfoDialog.vue";
 
 export default {
-  
+  name: "App",
   data() {
     return {
       tlItems: [
-        {name: 'Coax', component: 'Coax'},
-        {name: 'Microstrip', component: 'Microstrip'},
-        {name: 'Symmetric Stripline', component: 'SymmStripline'},
-        {name: 'Coplanar Waveguide', component: 'CPW'},
-        {name: 'Grounded Coplanar Waveguide', component: 'GCPW'},
+        { name: "Coax", component: "Coax" },
+        { name: "Microstrip", component: "Microstrip" },
+        { name: "Symmetric Stripline", component: "SymmStripline" },
+        { name: "CPW", component: "CPW" },
+        { name: "Grounded CPW", component: "GCPW" }
       ],
-      tlType: 'Coax',
-      dialog: false
+      tlType: "Coax",
+      dialog: false,
+      isMobile: true
     };
   },
   components: {
@@ -59,8 +65,20 @@ export default {
     CPW,
     GCPW,
     InfoDialog
+  },
+  methods: {
+    resize() {
+      this.isMobile = window.innerWidth < 350;
+    }
+  },
+  created: function() {
+    this.isMobile = window.innerWidth < 350;
+    window.addEventListener("resize", this.resize);
+  },
+  destroyed: function() {
+    window.removeEventListener("resize", this.resize);
   }
-}
+};
 </script>
 
 
